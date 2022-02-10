5ba9f16cdcecf0a21fd4395baf748a037736b00e
ljl <1601029287@qq.com>
2022-02-10 13:47:42 +0800

    hospital_manager

---
+++ yygh_parent/hospital-manager/src/main/java/com/exp/hmanage/hospitalmanage/mapper/ScheduleMapper.java
@@ -0,0 +1,10 @@
+package com.exp.hmanage.hospitalmanage.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.exp.hmanage.hospitalmanage.model.Schedule;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ScheduleMapper extends BaseMapper<Schedule> {
+
+}
