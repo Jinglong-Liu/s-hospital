2731a2802d34b14b7dfb6bebabaa5dd6f50e7520
ljl <1601029287@qq.com>
2022-02-11 10:44:33 +0800

    register nacos

--- yygh_parent/service/service_hosp/src/main/java/com/exp/hospital/hosp/ServiceHospApplication.java
+++ yygh_parent/service/service_hosp/src/main/java/com/exp/hospital/hosp/ServiceHospApplication.java
@@ -3,10 +3,12 @@ package com.exp.hospital.hosp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.exp.hospital")
+@EnableDiscoveryClient
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class, args);
